38d898a030ca53c59731daed0e7bdd79af03cceb
Phuocanh24 <daudau24012006@gmail.com>
2026-06-03 19:59:57 +0700

    fix đợt 1

---
+++ HAVENIX/Migrations/202606031249312_AddMovieStatus.cs
@@ -0,0 +1,18 @@
+﻿namespace HAVENIX.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddMovieStatus : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Movies", "Status", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Movies", "Status");
+        }
+    }
+}
--- HAVENIX/Models/Movie.cs
+++ HAVENIX/Models/Movie.cs
@@ -20,6 +20,8 @@ namespace havenix.Models
         public string Description { get; set; }
         public string Language { get; set; }
 
+        public string Status { get; set; }
+
         public virtual ICollection<Showtime> Showtimes { get; set; }
     }
 }
