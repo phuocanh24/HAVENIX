5b14b2d923956eba12c56aa62593d4572a2f21b5
Phuocanh24 <daudau24012006@gmail.com>
2026-06-05 23:26:48 +0700

    fix

---
+++ HAVENIX/Controllers/BookingController.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+using havenix.Data;
+
+namespace HAVENIX.Controllers
+{
+    public class BookingController : Controller
+    {
+        private HavenixDbContext db = new HavenixDbContext();
+
+        public ActionResult Create(int showtimeId)
+        {
+            var showtime = db.Showtimes
+                .Include(x => x.Movie)
+                .FirstOrDefault(x => x.Id == showtimeId);
+
+            if (showtime == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(showtime);
+        }
+    }
+}
--- HAVENIX/Controllers/HomeController.cs
+++ HAVENIX/Controllers/HomeController.cs
@@ -1,75 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
+using havenix.Data;
+using System.Data.Entity;
 
 namespace HAVENIX.Controllers
 {
     public class HomeController : Controller
     {
+        private HavenixDbContext db = new HavenixDbContext();
+
         public ActionResult Index()
         {
-            return View();
+            var movies = db.Movies
+                .Include(x => x.Showtimes)
+                .OrderBy(x => x.Id)
+                .ToList();
+
+            return View(movies);
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
-        public ActionResult Online()
-        {
-            return View();
-        }
-
-        public ActionResult GiftCard()
-        {
-            return View();
-        }
-
-        public ActionResult Recruitment()
-        {
-            return View();
-        }
-
-        public ActionResult Advertising()
-        {
-            return View();
-        }
-
-        public ActionResult Terms()
-        {
-            return View();
-        }
-
-        public ActionResult Transaction()
-        {
-            return View();
-        }
-
-        public ActionResult Payment()
-        {
-            return View();
-        }
-
-        public ActionResult Privacy()
-        {
-            return View();
-        }
-
-        public ActionResult Rules()
-        {
-            return View();
-        }
+        public ActionResult About() { return View(); }
+        public ActionResult Contact() { return View(); }
+        public ActionResult Online() { return View(); }
+        public ActionResult GiftCard() { return View(); }
+        public ActionResult Recruitment() { return View(); }
+        public ActionResult Advertising() { return View(); }
+        public ActionResult Terms() { return View(); }
+        public ActionResult Transaction() { return View(); }
+        public ActionResult Payment() { return View(); }
+        public ActionResult Privacy() { return View(); }
+        public ActionResult Rules() { return View(); }
     }
 }
